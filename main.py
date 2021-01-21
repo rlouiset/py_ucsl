ca8e42dd4b4d85beef181a85e579d4dee3ceb6ba
Robin Louiset <robin76lt@gmail.com>
2021-01-21 17:35:49 +0100

    Update code

--- main.py
+++ main.py
@@ -246,11 +246,12 @@ class HYDRA(BaseML):
             ##
             cluster_barycenters =  self.barycenters[idx_outside_polytope]
             boundary_baricenters_scores = np.zeros((S.shape))
+            mean_b = np.mean([self.coefficients[idx_outside_polytope][cluster_i] for cluster_i in self.n_clusters_per_label], 0)
+            mean_w = np.mean([self.intercepts[idx_outside_polytope][cluster_i] for cluster_i in self.n_clusters_per_label], 0)
+            mean_w_norm = mean_w / np.linalg.norm(mean_w) ** 2
+
             for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope]) :
-                w_cluster_i = self.coefficients[idx_outside_polytope][cluster_i]
-                b_cluster_i = self.intercepts[idx_outside_polytope][cluster_i]
-                w_cluster_i_norm = w_cluster_i / np.linalg.norm(w_cluster_i)**2
-                boundary_barycenter_i = cluster_barycenters[cluster_i] + (cluster_barycenters[cluster_i]@w_cluster_i[0]+b_cluster_i)*w_cluster_i_norm
+                boundary_barycenter_i = cluster_barycenters[cluster_i] + (cluster_barycenters[cluster_i]@mean_w[0]+mean_b)*mean_w_norm
                 boundary_baricenters_scores[:,cluster_i] = np.linalg.norm((X-boundary_barycenter_i), axis=1)
             # compute closest assigned hyperpan normal drection
             Q = py_softmax(-boundary_baricenters_scores[index], 1)
