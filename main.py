73e59cbbf2c15160073c16ca9db35e0125aee30d
Robin Louiset <robin76lt@gmail.com>
2021-01-25 17:12:55 +0100

    Update code

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ class HYDRA(BaseML):
                     w_cluster_i = self.coefficients[label][cluster_i]
                     b_cluster_i = self.intercepts[label][cluster_i]
                     w_cluster_i_norm = w_cluster_i / np.linalg.norm(w_cluster_i)**2
-                    boundary_barycenter_i =  self.barycenters[label][cluster_i] + (self.barycenters[label][cluster_i]@w_cluster_i[0]+b_cluster_i)*w_cluster_i_norm
+                    boundary_barycenter_i = self.barycenters[label][cluster_i] + (self.barycenters[label][cluster_i]@w_cluster_i[0]+b_cluster_i)*w_cluster_i_norm
                     barycenters_scores_dict[label][:,cluster_i] = sigmoid(-np.linalg.norm(X-boundary_barycenter_i, axis=1))
                 for cluster_i in range(self.n_clusters_per_label[label]):
                     cluster_predictions[label][:, cluster_i+1] = barycenters_scores_dict[label][:, cluster_i] / np.sum(barycenters_scores_dict[label], 1)     # P(cluster=i|y=label)
@@ -183,7 +183,7 @@ class HYDRA(BaseML):
                 S_hold = S.copy()
                 S, cluster_index = self.update_S(X, y, S, index_positives, cluster_index, idx_outside_polytope)
                 self.S_lists[idx_outside_polytope][1+iter]=S.copy()
-                #S[index_negatives, :] = 1/n_clusters
+                S[index_negatives, :] = 1/n_clusters
                 S[index_positives, :] = 0
                 S[index_positives, cluster_index[index_positives]] = 1
 
@@ -228,7 +228,7 @@ class HYDRA(BaseML):
             cluster_index[index] = 0
             return S, cluster_index
 
-        Q = S #[index]
+        Q = S[index]
         if self.clustering_strategy == 'original':
             svm_scores = np.zeros(S.shape)
             for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope]) :
@@ -265,34 +265,14 @@ class HYDRA(BaseML):
             directions = directions / (np.linalg.norm(directions, axis=1)**2)[:, None]
 
             mean_direction = (directions[0] - directions[1])/2
-            mean_direction = mean_direction - (np.dot(mean_direction, SVM_coefficient_norm[0]) * SVM_coefficient_norm[0])
+            #mean_direction = mean_direction - (np.dot(mean_direction, SVM_coefficient_norm[0]) * SVM_coefficient_norm[0])
             mean_intercept = 0
 
             X_proj = (np.matmul(mean_direction[None,:], X.transpose()) + mean_intercept).transpose().squeeze()
-            X_proj = sigmoid(X_proj[:, None]*3/np.max(X_proj))
+            X_proj = sigmoid(X_proj[:, None]*5/np.max(X_proj))
 
-            Q = np.concatenate((X_proj, 1-X_proj), axis=1) #[index]
+            Q = np.concatenate((1-X_proj, X_proj), axis=1)[index]
             #Q = cpu_sk(Q, lambda_=0.1)
-            #Q = np.rint(Q)
-
-        elif self.clustering_strategy in ['mean_hp_sk']:
-            SVM_coefficient, SVM_intercept = self.launch_svc(X, y, sample_weight=None, kernel='linear')
-            SVM_coefficient_norm = SVM_coefficient / np.linalg.norm(SVM_coefficient) ** 2
-
-            directions = np.array([self.coefficients[idx_outside_polytope][cluster_i][0] for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope])])
-            #intercepts = np.array([self.coefficients[idx_outside_polytope][cluster_i][0] for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope])])
-
-            directions = directions / (np.linalg.norm(directions, axis=1)**2)[:, None]
-
-            mean_direction = (directions[0] - directions[1])/2
-            mean_direction = mean_direction - (np.dot(mean_direction, SVM_coefficient_norm[0]) * SVM_coefficient_norm[0])
-            mean_intercept = 0
-
-            X_proj = (np.matmul(mean_direction[None,:], X.transpose()) + mean_intercept).transpose().squeeze()
-            X_proj = sigmoid(X_proj[:, None]*3/np.max(X_proj))
-
-            Q = np.concatenate((X_proj, 1-X_proj), axis=1) #[index]
-            Q = cpu_sk(Q, lambda_=1)
             #Q = np.rint(Q)
 
         elif self.clustering_strategy == 'boundary_barycenter':
@@ -308,8 +288,7 @@ class HYDRA(BaseML):
             # compute closest assigned hyperpan normal drection
             Q = py_softmax(-boundary_baricenters_scores[index], 1)
 
-        #S[index, :] = Q
-        S = Q
+        S[index, :] = Q
         cluster_index[index] = np.argmax(Q[index], axis=1)
         return S, cluster_index
 
