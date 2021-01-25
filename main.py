c95c00e2a488bea2982afb19a8ca6565cb4a5b83
Robin Louiset <robin76lt@gmail.com>
2021-01-25 13:23:53 +0100

    Update code

--- main.py
+++ main.py
@@ -241,8 +241,8 @@ class HYDRA(BaseML):
         elif self.clustering_strategy in ['direction']:
             directions = np.array([self.coefficients[idx_outside_polytope][cluster_i][0] for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope])])
             print(directions.shape)
-            directions, R = np.linalg.qr(directions.T)
-            print(R.shape)
+            directions_ortho, R = np.linalg.qr(directions)
+            directions = directions_ortho@directions
             print(directions.shape)
             print('')
 
