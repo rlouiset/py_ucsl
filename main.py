a370bc7ed462052e415f7c48e1b39a149729a283
Robin Louiset <robin76lt@gmail.com>
2021-01-26 17:54:57 +0100

    Update code

--- main.py
+++ main.py
@@ -448,6 +448,8 @@ class HYDRA(BaseML):
             PCA_ = PCA(n_components=2)
             self.cluster_estimators[idx_outside_polytope]['directions'] = PCA_.fit_transform(consensus_direction)
 
+            print(self.cluster_estimators[idx_outside_polytope]['directions'])
+
             self.cluster_estimators[idx_outside_polytope]['K-means'] = GaussianMixture(n_components=n_clusters).fit(X[index_positives]@self.cluster_estimators[idx_outside_polytope]['directions'])
             consensus_scores = self.cluster_estimators[idx_outside_polytope]['K-means'].predict_proba(X@self.cluster_estimators[idx_outside_polytope]['directions'])
 
