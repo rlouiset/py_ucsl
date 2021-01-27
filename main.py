f30b4ff68bc0e432f850bd97a48b40d936e4c6af
Robin Louiset <robin76lt@gmail.com>
2021-01-27 17:38:32 +0100

    Update code

--- main.py
+++ main.py
@@ -331,7 +331,9 @@ class HYDRA(BaseML):
             X_proj = (np.matmul(mean_direction[None,:], X_norm.transpose()) + mean_intercept).transpose().squeeze()
             X_proj = sigmoid(X_proj[:, None]*5/np.max(X_proj))
 
-            if balanced_accuracy_score(S[:,1], np.rint(X_proj)) > balanced_accuracy_score(S[:,1], np.rint(1-X_proj)) :
+            X_pred = np.rint(X_proj).astype(np.int)
+
+            if balanced_accuracy_score(np.argmax(S[index],1), X_pred) > balanced_accuracy_score(np.argmax(S[index],1), 1-X_pred) :
                 Q = np.concatenate((1-X_proj, X_proj), axis=1)
             else :
                 Q = np.concatenate((X_proj, 1-X_proj), axis=1)
