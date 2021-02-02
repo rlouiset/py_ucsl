8ec7f74dd1acc29351f8f0e0f3fd2fb8dfddf640
Robin Louiset <robin76lt@gmail.com>
2021-02-02 14:22:24 +0100

    Update code

--- main.py
+++ main.py
@@ -354,10 +354,10 @@ class HYDRA(BaseML):
 
         if self.consensus == 'w_original':
             ## clustering relevancy
-            w_clusterings = np.array([0]*consensus_assignment.shape[1])
+            w_clusterings = np.zeros((consensus_assignment.shape[1], consensus_assignment.shape[1]))
             for clustering_i in range(len(w_clusterings)) :
                 for clustering_j in range(len(w_clusterings)) :
-                    w_clusterings[clustering_i] += ARI(consensus_assignment[clustering_i], consensus_assignment[clustering_j])
+                    w_clusterings[clustering_i][clustering_j] = ARI(consensus_assignment[clustering_i], consensus_assignment[clustering_j])
             print(w_clusterings)
             w_clusterings[w_clusterings<0] = 0
             w_clusterings = np.array(w_clusterings) / np.sum(w_clusterings)
