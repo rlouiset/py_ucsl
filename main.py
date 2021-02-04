2aa0a11fd8a132de9c133d9a9bb9d8a1faeed2be
Robin Louiset <robin76lt@gmail.com>
2021-02-04 11:31:57 +0100

    Update code

--- main.py
+++ main.py
@@ -270,8 +270,8 @@ class HYDRA(BaseML):
                 b_cluster_i = self.intercepts[idx_outside_polytope][cluster_i]
                 w_cluster_i_norm = w_cluster_i / np.linalg.norm(w_cluster_i) ** 2
                 X_proj_i = X - (X @ w_cluster_i.T + b_cluster_i) * np.repeat(w_cluster_i_norm, X.shape[0], axis=0)
-                X_proj += 0.5 * S[:, cluster_i] * X_proj_i
-            centroids = [np.mean(S[index, cluster_i]*X_proj[index,:], 0) for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope])]
+                X_proj += 0.5 * X_proj_i
+            centroids = [np.mean(S[index, cluster_i][:,None]*X_proj[index,:], 0) for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope])]
             for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope]):
                 centroid_scores[:,cluster_i] = np.linalg.norm((X_proj-centroids[cluster_i]), axis=1)
             Q = py_softmax(1/centroid_scores, 1)
