6e7f2af040d15ac41abf79b517dee1a0ebe98e90
Robin Louiset <robin76lt@gmail.com>
2021-02-04 14:07:05 +0100

    Update code

--- main.py
+++ main.py
@@ -276,6 +276,7 @@ class HYDRA(BaseML):
                 X_proj += 0.5 * X_proj_i
             self.X_proj_list[idx_outside_polytope].append(X_proj.copy())
             centroids = [np.mean(S[index, cluster_i][:,None]*X_proj[index,:], 0) for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope])]
+            print(centroids)
             #for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope]):
             #    centroid_scores[:,cluster_i] = np.linalg.norm((X_proj-centroids[cluster_i]), axis=1)
             GMM = GaussianMixture(n_components=2, means_init=np.array(centroids)).fit(X_proj[index])
