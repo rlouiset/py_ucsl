8e54b8376b4cbda0a7c2b060753e014cca6e5c01
Robin Louiset <robin76lt@gmail.com>
2021-02-04 13:40:49 +0100

    Update code

--- main.py
+++ main.py
@@ -280,7 +280,8 @@ class HYDRA(BaseML):
             centroids = [np.mean(S[index, cluster_i][:,None]*X_proj[index,:], 0) for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope])]
             #for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope]):
             #    centroid_scores[:,cluster_i] = np.linalg.norm((X_proj-centroids[cluster_i]), axis=1)
-            Q = one_hot_encode(KMeans(n_clusters=2, init=np.array(centroids), n_init=1).fit_predict(X_proj))
+            GMM = GaussianMixture(n_components=2, init_params=np.array(centroids), covariance_type='diag').fit(X_proj[index])
+            Q = GMM.predict_proba(X_proj)
 
 
         elif self.clustering_strategy in ['mean_hp', 'nw_mean_hp']:
