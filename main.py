86caa9aa862e4109f45398eb4e39f72bb269031a
Robin Louiset <robin76lt@gmail.com>
2021-02-09 18:04:26 +0100

    Update code

--- main.py
+++ main.py
@@ -211,7 +211,6 @@ class HYDRA(BaseML):
                         print("Re-initialization of the clustering...")
                         S, cluster_index = self.init_S(X, y_polytope, index_positives, index_negatives, self.n_clusters_per_label[idx_outside_polytope], idx_outside_polytope, initialization_type=self.initialization_type)
                     if np.count_nonzero(S[index_negatives, cluster_i]) == 0 :
-                        print('wrong')
                         S[index_negatives, cluster_i] = 1/self.n_clusters_per_label[idx_outside_polytope]
 
                 for cluster_i in range(self.n_clusters_per_label[idx_outside_polytope]):
@@ -251,12 +250,10 @@ class HYDRA(BaseML):
 
             basis, norms = [], []
             mean_norm_dir = np.mean(np.linalg.norm(directions, 1))
-            print('mean norm : ', mean_norm_dir)
             for v in directions:
                 w = v - np.sum(np.dot(v, b) * b for b in basis)
                 #if (np.abs(w) > 1e-3).any() :
-                if (np.linalg.norm(w)/mean_norm_dir) > 1e-2 :
-                    print(np.linalg.norm(w))
+                if np.linalg.norm(w) > 0.2 :
                     basis.append(w / np.linalg.norm(w))
                 else :
                     print('Rejected : ', np.linalg.norm(w))
