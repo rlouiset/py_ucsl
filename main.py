a245ad237aa5312a29bd0d0c351185590ee46c9f
Robin Louiset <robin76lt@gmail.com>
2021-02-09 16:30:22 +0100

    Update code

--- main.py
+++ main.py
@@ -251,7 +251,7 @@ class HYDRA(BaseML):
             basis, norms = [], []
             for v in directions:
                 w = v - np.sum(np.dot(v, b) * b for b in basis)
-                if (np.abs(w) > 1e-10).any():
+                if (np.abs(w) > 1e-5).any():
                     basis.append(w / np.linalg.norm(w))
                 else :
                     print(w)
