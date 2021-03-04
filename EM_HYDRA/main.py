2776d6ea5ccad4ede51b564d1fd5014817c99682
Robin Louiset <robin76lt@gmail.com>
2021-03-04 18:41:24 +0100

    Update code

--- EM_HYDRA/main.py
+++ EM_HYDRA/main.py
@@ -639,6 +639,7 @@ class HYDRA(BaseEM, ClassifierMixin):
                     S, cluster_index = self.initialize_clustering(X, y_polytope, index_positives, index_negatives,
                                                                   n_clusters, idx_outside_polytope)
 
+                print(np.max(S[index_negatives, cluster]))
                 if np.max(S[index_negatives, cluster]) < 0.01:
                     print(
                         "Cluster too far, one cluster have no negative points anymore, in consensus : %d" % (
