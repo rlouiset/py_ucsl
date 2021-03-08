4b556243f24da12740b4d694bfd37f9ef5fb4fc2
Robin Louiset <robin76lt@gmail.com>
2021-03-08 14:52:58 +0100

    Update code

--- EM_HYDRA/main.py
+++ EM_HYDRA/main.py
@@ -646,11 +646,7 @@ class HYDRA(BaseEM, ClassifierMixin):
                         "Cluster too far, one cluster have no negative points anymore, in consensus : %d" % (
                                 iteration - 1))
                     logging.debug("Re-distribution of this cluster negative weight to 'all'...")
-                    if self.clustering == "k_means":
-                        S[index_negatives, cluster] = 1 / n_clusters
-                    else:
-                        S[index_negatives, cluster] = S[index_negatives, cluster] * (1 / n_clusters) / np.sum(
-                            S[index_negatives, cluster])
+                    S[index_negatives, cluster] = 1 / n_clusters
 
             for cluster in range(n_clusters):
                 cluster_assignment = np.ascontiguousarray(S[:, cluster])
