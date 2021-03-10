42512d2b673ef43829d3ec39a8e89261c427b05d
Robin Louiset <robin76lt@gmail.com>
2021-03-10 10:22:59 +0100

    Update code

--- EM_HYDRA/main.py
+++ EM_HYDRA/main.py
@@ -667,7 +667,7 @@ class HYDRA(BaseEM, ClassifierMixin):
             S_hold = S.copy()
             S, cluster_index, n_clusters = self.expectation_step(X, S, index_positives, idx_outside_polytope, n_clusters, consensus)
 
-            print(np.argmax(S[index_positives]))
+            print(np.argmax(S[index_positives], 1))
 
             # applying the negative weighting set as input
             if self.negative_weighting in ['all']:
