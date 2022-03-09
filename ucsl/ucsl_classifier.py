88b68799faf0b219376bdd3b6653b69b3f50495c
Robin Louiset <robin76lt@gmail.com>
2022-03-09 15:34:13 +0100

    k-means method fix

--- ucsl/ucsl_classifier.py
+++ ucsl/ucsl_classifier.py
@@ -413,7 +413,7 @@ class UCSL_C(BaseEM, ClassifierMixin):
             KM_barycenters = self.clustering_method[consensus].cluster_centers_
             Q = np.ones((len(X_proj), self.n_clusters)) / self.n_clusters
             for cluster in range(self.n_clusters):
-                S[:, cluster] = np.sum(np.abs(X_proj - KM_barycenters[cluster][None, :]), 1)
+                Q[:, cluster] = np.sum(np.abs(X_proj - KM_barycenters[cluster][None, :]), 1)
             Q = - Q
             Q = Q + np.sum(Q, axis=1, keepdims=True)
             Q = Q / np.sum(Q, 1)[:, None]
